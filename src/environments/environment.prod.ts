b0f3e363f652891aa6c2cde18e73038aade02711
FakerAle <alejothebest01@gmail.com>
2021-03-28 00:07:20 -0500

    Ajustes githup pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig :{
+    apiKey: "AIzaSyCNa8LnqoWUuWSwEp6aovDgBF56Ftz629E",
+    authDomain: "ngblog-98663.firebaseapp.com",
+    projectId: "ngblog-98663",
+    storageBucket: "ngblog-98663.appspot.com",
+    messagingSenderId: "789995662071",
+    appId: "1:789995662071:web:50a1aabb1a4de31a8601f2",
+    measurementId: "G-TC9XH1RNB0"
+  }
 };
