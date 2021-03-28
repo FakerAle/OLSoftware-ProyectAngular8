de5ec3907237930dd7923bd3be59085415699d07
FakerAle <alejothebest01@gmail.com>
2021-03-28 00:05:52 -0500

    Auto-generated commit

---
+++ SofwarePrueba/common-es5.js
@@ -0,0 +1,47 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["common"],{
+
+/***/ "./src/app/shared/services/auth.service.ts":
+/*!*************************************************!*\
+  !*** ./src/app/shared/services/auth.service.ts ***!
+  \*************************************************/
+/*! exports provided: AuthService */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "AuthService", function() { return AuthService; });
+/* harmony import */ var tslib__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! tslib */ "./node_modules/tslib/tslib.es6.js");
+/* harmony import */ var _angular_core__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @angular/core */ "./node_modules/@angular/core/fesm5/core.js");
+/* harmony import */ var _angular_fire_auth__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @angular/fire/auth */ "./node_modules/@angular/fire/auth/index.js");
+
+
+
+var AuthService = /** @class */ (function () {
+    function AuthService(afAuth) {
+        this.afAuth = afAuth;
+        this.userData = afAuth.authState;
+    }
+    AuthService.prototype.loginByEmail = function (user) {
+        var email = user.email, password = user.password;
+        return this.afAuth.auth.signInWithEmailAndPassword(email, password);
+    };
+    AuthService.prototype.logout = function () {
+        this.afAuth.auth.signOut();
+    };
+    AuthService.ctorParameters = function () { return [
+        { type: _angular_fire_auth__WEBPACK_IMPORTED_MODULE_2__["AngularFireAuth"] }
+    ]; };
+    AuthService = tslib__WEBPACK_IMPORTED_MODULE_0__["__decorate"]([
+        Object(_angular_core__WEBPACK_IMPORTED_MODULE_1__["Injectable"])({
+            providedIn: 'root'
+        })
+    ], AuthService);
+    return AuthService;
+}());
+
+
+
+/***/ })
+
+}]);
+//# sourceMappingURL=common-es5.js.map
